3c33dea996d760a0968bbaaa288dedd2e4f39a1a
shiraosa <tezukatrigger@gmail.com>
2020-08-21 05:27:11 +0900

    定義書に従いバリデーション追加

--- html/mycakeapp/src/Model/Table/BiditemsTable.php
+++ html/mycakeapp/src/Model/Table/BiditemsTable.php
@@ -74,6 +74,18 @@ class BiditemsTable extends Table
             ->notEmptyString('name');
 
         $validator
+            ->scalar('detail')
+            ->maxLength('detail', 1000)
+            ->requirePresence('detail', 'create')
+            ->notEmptyString('detail');
+
+        $validator
+            ->scalar('image_path')
+            ->maxLength('image_path', 100)
+            ->requirePresence('image_path', 'create')
+            ->notEmptyString('image_path');
+
+        $validator
             ->boolean('finished')
             ->requirePresence('finished', 'create')
             ->notEmptyString('finished');
