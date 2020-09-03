133b40014516ea4da744ee5f99d9d3d0f5b2e803
shiraosa <tezukatrigger@gmail.com>
2020-09-04 07:48:54 +0900

    マイグレーションファイルの作成

---
+++ html/mycakeapp/config/Migrations/20200901221335_CreateShippings.php
@@ -0,0 +1,54 @@
+<?php
+use Migrations\AbstractMigration;
+
+class CreateShippings extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('shippings');
+        $table->addColumn('bidinfo_id', 'integer', [
+            'default' => null,
+            'limit' => 11,
+            'null' => false,
+        ]);
+        $table->addColumn('name', 'string', [
+            'default' => null,
+            'limit' => 100,
+            'null' => false,
+        ]);
+        $table->addColumn('address', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->addColumn('phone_number', 'string', [
+            'default' => null,
+            'limit' => 13,
+            'null' => false,
+        ]);
+        $table->addColumn('is_shipped', 'boolean', [
+            'default' => 0,
+            'null' => false,
+        ]);
+        $table->addColumn('is_received', 'boolean', [
+            'default' => 0,
+            'null' => false,
+        ]);
+        $table->addColumn('created', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addColumn('modified', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->create();
+    }
+}
---
+++ html/mycakeapp/config/Migrations/20200901222139_CreateRatings.php
@@ -0,0 +1,51 @@
+<?php
+use Migrations\AbstractMigration;
+
+class CreateRatings extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('ratings');
+        $table->addColumn('rated_user_id', 'integer', [
+            'default' => null,
+            'limit' => 11,
+            'null' => false,
+        ]);
+        $table->addColumn('rated_by_user_id', 'integer', [
+            'default' => null,
+            'limit' => 11,
+            'null' => false,
+        ]);
+        $table->addColumn('bidinfo_id', 'integer', [
+            'default' => null,
+            'limit' => 11,
+            'null' => false,
+        ]);
+        $table->addColumn('rating', 'integer', [
+            'default' => null,
+            'limit' => 1,
+            'null' => false,
+        ]);
+        $table->addColumn('comment', 'string', [
+            'default' => null,
+            'limit' => 1000,
+            'null' => false,
+        ]);
+        $table->addColumn('created', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addColumn('modified', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->create();
+    }
+}
